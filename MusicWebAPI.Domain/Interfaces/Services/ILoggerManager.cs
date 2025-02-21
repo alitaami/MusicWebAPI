c23cd76c47a4b4ed49761706c1cdf1f360121950
ali <alitaami81@gmail.com>
2025-02-21 15:24:32 +0330

    Added LoggerManager

---
+++ MusicWebAPI.Domain/Interfaces/Services/ILoggerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWebAPI.Domain.Interfaces.Services
+{
+    // /Domain/Interfaces/ILoggerManager.cs
+    namespace MusicWebAPI.Domain.Interfaces
+    {
+        public interface ILoggerManager
+        {
+            void LogInfo(string message);
+            void LogWarn(string message);
+            void LogError(string message);
+            void LogDebug(string message);
+        }
+    }
+
+}
---
+++ MusicWebAPI.Infrastructure/Logging/LoggerManager.cs
@@ -0,0 +1,44 @@
+﻿using MusicWebAPI.Domain.Interfaces.Services.MusicWebAPI.Domain.Interfaces;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicWebAPI.Infrastructure.Logging
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private readonly ILogger _logger;
+
+        public LoggerManager()
+        {
+            // Initialize the logger with basic configuration (console and file sinks as examples)
+            _logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/musicwebapi_log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            _logger.Warning(message);
+        }
+
+        public void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+    }
+}
