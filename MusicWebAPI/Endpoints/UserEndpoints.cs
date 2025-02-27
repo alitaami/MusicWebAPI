f469c176c57000f18bff4d73287b2e7a68c04e75
ali <alitaami81@gmail.com>
2025-02-28 02:56:47 +0330

    Added 'UserEndpoints'

---
+++ MusicWebAPI/Endpoints/UserEndpoints.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using MusicWebAPI.Application.Commands;
+using MusicWebAPI.Domain.Entities;
+using MusicWebAPI.API.Base;
+
+namespace MusicWebAPI.API.Endpoints
+{
+    public class UserEndpoints : ApiResponseBase
+    {
+        public static void RegisterUserEndpoints(WebApplication app)
+        {
+            // Register user endpoint
+            app.MapPost("/api/register", async (IMediator mediator, RegisterUserCommand command) =>
+            {
+                try
+                {
+                    var user = await mediator.Send(command);
+
+                    // Return success response wrapped in ApiResult
+                    return new UserEndpoints().Ok(user);
+                }
+                catch (Exception ex)
+                { 
+                    return new UserEndpoints().BadRequest<User>(ex.Message);
+                }
+            })
+            .WithName("RegisterUser")
+            .Produces<User>(StatusCodes.Status200OK)
+            .Produces<string>(StatusCodes.Status400BadRequest)
+            .WithTags("User");
+
+            // Login user endpoint
+            app.MapPost("/api/login", async (IMediator mediator, LoginUserCommand command) =>
+            {
+                try
+                {
+                    // getting JWT token
+                    var token = await mediator.Send(command);
+
+                    // Return success response wrapped in ApiResult
+                    return new UserEndpoints().Ok(new { Token = token });
+                }
+                catch (Exception ex)
+                { 
+                    return new UserEndpoints().BadRequest<string>(ex.Message);
+                }
+            })
+            .WithName("LoginUser")
+            .Produces<string>(StatusCodes.Status200OK)
+            .Produces<string>(StatusCodes.Status400BadRequest)
+            .WithTags("User");
+        }
+    }
+}
