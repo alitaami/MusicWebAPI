7a2e58c86b46012fad8d10ea7a2796d0d96b773d
ali <alitaami81@gmail.com>
2025-06-18 01:50:13 +0330

    Added tests for 'ForgetPassword'  & 'ResetPassword'

--- MusicWebAPI.UnitTests/Post-Implementations/UserTests.cs
+++ MusicWebAPI.UnitTests/Post-Implementations/UserTests.cs
@@ -7,6 +7,9 @@ using MusicWebAPI.Core.Resources;
 using MusicWebAPI.Application.Features.Properties.Auth.Commands.GoogleLogin;
 using MusicWebAPI.Application.Features.Properties.Auth.Commands.Login;
 using MusicWebAPI.Application.Features.Properties.Auth.Commands.Register;
+using MusicWebAPI.Application.Features.Properties.Auth.Commands.ForgetPassword;
+using MusicWebAPI.Domain.External.Caching;
+using MusicWebAPI.Application.Features.Properties.Auth.Commands.ResetPassword;
 
 namespace MusicWebAPI.UnitTests.TDD
 {
@@ -14,19 +17,25 @@ namespace MusicWebAPI.UnitTests.TDD
     public class UserTests
     {
         private Mock<IServiceManager> _serviceManagerMock;
+        private Mock<ICacheService> _cacheServiceMock;
         private Mock<IMapper> _mapperMock;
         private RegisterCommandHandler _registerUserHandler;
         private LoginCommandHandler _loginUserHandler;
         private GoogleLoginCommandHandler _googleLoginCommandHandler;
+        private ForgetPasswordCommandHandler _forgetPassCommandHandler;
+        private ResetPasswordCommandHandler _resetPassCommandHandler;
 
         [SetUp]
         public void Setup()
         {
             _serviceManagerMock = new Mock<IServiceManager>();
+            _cacheServiceMock = new Mock<ICacheService>();
             _mapperMock = new Mock<IMapper>();
             _registerUserHandler = new RegisterCommandHandler(_serviceManagerMock.Object, _mapperMock.Object);
             _loginUserHandler = new LoginCommandHandler(_serviceManagerMock.Object);
             _googleLoginCommandHandler = new GoogleLoginCommandHandler(_serviceManagerMock.Object);
+            _forgetPassCommandHandler = new ForgetPasswordCommandHandler(_serviceManagerMock.Object, _cacheServiceMock.Object);
+            _resetPassCommandHandler = new ResetPasswordCommandHandler(_serviceManagerMock.Object, _cacheServiceMock.Object);
         }
 
         [Test]
@@ -161,6 +170,158 @@ namespace MusicWebAPI.UnitTests.TDD
 
             // Assert
             _serviceManagerMock.Verify(service => service.User.GoogleLogin(It.IsAny<string>()), Times.Once);
+            Assert.AreEqual("Error", ex.Message);
+        }
+
+        [Test]
+        public async Task Handle_ForgetPassword_When_Successful()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+
+            _serviceManagerMock
+                   .Setup(service => service.User.ForgetPassword(email))
+                   .ReturnsAsync(otp);
+
+            // Act
+            await _forgetPassCommandHandler.Handle(new ForgetPasswordCommand(email), CancellationToken.None);
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ForgetPassword(email), Times.Once);
+        }
+
+        [Test]
+        public async Task Handle_ForgetPassword_When_UserNotFound()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+
+            _serviceManagerMock
+                 .Setup(service => service.User.ForgetPassword(email))
+                 .ThrowsAsync(new NotFoundException(Resource.UserNotFound));
+
+            // Act
+            var ex = Assert.ThrowsAsync<NotFoundException>(async () => await _forgetPassCommandHandler.Handle(new ForgetPasswordCommand(email), CancellationToken.None));
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ForgetPassword(email), Times.Once);
+            Assert.AreEqual(Resource.UserNotFound, ex.Message);
+        }
+
+        [Test]
+        public async Task Handle_ForgetPassword_When_NotSuccessful()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+
+            _serviceManagerMock
+                 .Setup(service => service.User.ForgetPassword(email))
+                 .ThrowsAsync(new Exception("Error"));
+
+            // Act
+            var ex = Assert.ThrowsAsync<Exception>(async () => await _forgetPassCommandHandler.Handle(new ForgetPasswordCommand(email), CancellationToken.None));
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ForgetPassword(email), Times.Once);
+            Assert.AreEqual("Error", ex.Message);
+        }
+
+        [Test]
+        public async Task Handle_ResetPassword_When_Successful()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+            string newPass = "@Litaami1985";
+            string cacheKey = $"{Resource.OtpCode_CacheKey}{email}";
+
+            _cacheServiceMock
+                .Setup(c => c.GetAsync<string>(cacheKey))
+                .ReturnsAsync(otp);
+
+            _serviceManagerMock
+                .Setup(service => service.User.ResetPassword(email, newPass))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            await _resetPassCommandHandler.Handle(new ResetPasswordCommand(email, otp, newPass), CancellationToken.None);
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ResetPassword(email, newPass), Times.Once);
+            _cacheServiceMock.Verify(c => c.RemoveAsync(cacheKey), Times.Once);
+        }
+
+
+        [Test]
+        public async Task Handle_ResetPassword_When_UserNotFound()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+            string newPass = "@Litaami1985";
+            string cacheKey = $"{Resource.OtpCode_CacheKey}{email}";
+
+            _cacheServiceMock
+                .Setup(c => c.GetAsync<string>(cacheKey))
+                .ReturnsAsync(otp);
+
+            _serviceManagerMock
+                 .Setup(service => service.User.ResetPassword(email, newPass))
+                 .ThrowsAsync(new NotFoundException(Resource.UserNotFound));
+
+            // Act
+            var ex = Assert.ThrowsAsync<NotFoundException>(async () => await _resetPassCommandHandler.Handle(new ResetPasswordCommand(email, otp, newPass), CancellationToken.None));
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ResetPassword(email, newPass), Times.Once);
+            Assert.AreEqual(Resource.UserNotFound, ex.Message);
+        }
+
+        [Test]
+        public async Task Handle_ResetPassword_When_OtpIsNullOrWrong()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+            string newPass = "@Litaami1985";
+
+            string cacheKey = $"{Resource.OtpCode_CacheKey}{email}";
+            _cacheServiceMock
+                .Setup(c => c.GetAsync<string>(cacheKey))
+                .ReturnsAsync((string?)null);
+
+            // Act
+            var ex = Assert.ThrowsAsync<LogicException>(async () => await _resetPassCommandHandler.Handle(new ResetPasswordCommand(email, otp, newPass), CancellationToken.None));
+
+            // Assert
+            Assert.AreEqual(Resource.WrongOtpCodeError, ex.Message);
+        }
+
+        [Test]
+        public async Task Handle_ResetPassword_When_NotSuccessful()
+        {
+            // Arrange
+            string email = "test@gmail.com";
+            string otp = "123456";
+            string newPass = "@Litaami1985";
+            string cacheKey = $"{Resource.OtpCode_CacheKey}{email}";
+
+            _cacheServiceMock
+                .Setup(c => c.GetAsync<string>(cacheKey))
+                .ReturnsAsync(otp);
+
+            _serviceManagerMock
+                 .Setup(service => service.User.ResetPassword(email, newPass))
+                 .ThrowsAsync(new Exception("Error"));
+
+            // Act
+            var ex = Assert.ThrowsAsync<Exception>(async () => await _resetPassCommandHandler.Handle(new ResetPasswordCommand(email, otp, newPass), CancellationToken.None));
+
+            // Assert
+            _serviceManagerMock.Verify(service => service.User.ResetPassword(email, newPass), Times.Once);
             Assert.AreEqual("Error", ex.Message);
         }
     }
